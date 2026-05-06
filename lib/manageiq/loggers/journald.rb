c40c8157b3cd280790c7cec8cf49f7d60ac2eab5
Adam Grare <adam@grare.com>
2026-05-06 14:01:50 -0400

    Add a method to get contents of the journal
    This method gets the contents of the journal from the end with a
    configurable max count.

--- lib/manageiq/loggers/journald.rb
+++ lib/manageiq/loggers/journald.rb
@@ -61,6 +61,27 @@ module ManageIQ
         )
       end
 
+      def contents(max_count = 1_000)
+        Systemd::Journal.open do |journal|
+          journal.filter(:syslog_identifier => progname)
+          journal.seek(:tail)
+          journal.move(-max_count)
+
+          results = []
+          loop do
+            entry = journal.current_entry
+            # This is the time in microseconds since the epoch UTC, formatted as a decimal string.
+            seconds_since_epoch = entry._source_realtime_timestamp.to_f / 1_000_000.0
+            timestamp = Time.zone.at(seconds_since_epoch).strftime("%Y-%m-%dT%H:%M:%S.%6N")
+
+            results << "[#{timestamp} ##{entry._pid}]#{entry.message}"
+            break unless journal.move_next
+          end
+
+          results
+        end
+      end
+
       private
 
       # Map the Systemd::Journal error levels to the Logger error levels. For
